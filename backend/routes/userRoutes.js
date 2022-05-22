41687166c49d6bb23fb62c8025834554d58a8a3a
Davide Valguarnera <davide.valguarnera@yahoo.it>
2022-05-22 21:50:30 +0200

    Added registration and password hashing, used mongo pre-save function instead of directly hash in the controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js';
 //import Product from '../models/productModel.js';
 import { protect } from '../middleware/authMiddleware.js';
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/login', authUser);
 // With the middleware created now I can simply protect any routes adding the protect
 router.route('/profile').get(protect, getUserProfile);
+router.route('/').post(registerUser);
 
 export default router;
