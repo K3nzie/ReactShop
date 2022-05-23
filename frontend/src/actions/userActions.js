a4d9cc726aabe98f2cba4b0d04a32fc6f6b47e97
Davide Valguarnera <davide.valguarnera@yahoo.it>
2022-05-23 12:44:00 +0200

    Added logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-  USER_LOGIN_FAIL
+  USER_LOGIN_FAIL,
+  USER_LOGOUT
 } from '../constants/userConstants.js';
 
 export const login = (email, password) => async (dispatch) => {
@@ -33,4 +34,9 @@ export const login = (email, password) => async (dispatch) => {
       payload: error.response && error.response.data.message ? error.response.data.message : error.message
     })
   }
+};
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo');
+    dispatch({ type: USER_LOGOUT });
 }
